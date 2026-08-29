44bf7dbdc090ac328276cf2bfecc5784bf444a89
RohanPGP <159392534+RohanPGP@users.noreply.github.com>
2026-08-29 17:01:51 -0500

    Add About page route and Footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider, useAuth } from './hooks/useAuth';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,6 +9,7 @@ import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import Upload from './pages/Upload';
 import StudyPackView from './pages/StudyPackView';
+import About from './pages/About';
 import './styles/global.css';
 
 function AppRoutes() {
@@ -22,6 +24,7 @@ function AppRoutes() {
         <Route path="/" element={user ? <Navigate to="/dashboard" replace /> : <Home />} />
         <Route path="/login" element={user ? <Navigate to="/dashboard" replace /> : <Login />} />
         <Route path="/signup" element={user ? <Navigate to="/dashboard" replace /> : <Signup />} />
+        <Route path="/about" element={<About />} />
 
         <Route path="/payment" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={
@@ -36,6 +39,7 @@ function AppRoutes() {
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      <Footer />
     </>
   );
 }
